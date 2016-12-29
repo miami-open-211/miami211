609a427f2b4e15bb4a54d452d5e27ff8177ea28a
davidjamesknight <davidjamesknight@gmail.com>
2016-12-29 14:34:56 -0500

    Initial

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "sinatra-contrib"
+gem 'ohanakapa', '~> 1.1.1'
---
+++ server.rb
@@ -0,0 +1,24 @@
+system("clear")
+
+require "pry"
+require "ohanakapa"
+require "sinatra"
+require "sinatra/reloader" if development?
+
+# ===========================
+
+get "/" do
+    erb(:search)
+end
+
+get "/org_search" do # form method=GET => /org_search?search_terms
+    
+    Ohanakapa.configure do |config|
+        config.api_token = "7a7031966c424391bbab9900fcf3fa0a" # = ENV['OHANA_API_TOKEN'] if ENV['OHANA_API_TOKEN'].present?
+        config.api_endpoint = "https://ohana-api.herokuapp.com/api/" # = ENV['OHANA_API_ENDPOINT']
+    end
+    
+    @search = Ohanakapa.search("search", :keyword => params[:search_terms])
+    print @search
+    erb(:show_search_results)
+end
