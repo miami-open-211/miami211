f0a92c5989d833f24eb3fc8ff9d1767bdc008d78
davidjamesknight <davidjamesknight@gmail.com>
2016-12-30 16:50:06 -0500

    Add search refine parameters; populate search results; basic CSS

--- server.rb
+++ server.rb
@@ -7,16 +7,45 @@ require "sinatra/reloader" if development?
 # ===========================
 
 get "/" do
+    @hide_search = true
     erb(:search)
 end
 
 get "/org_search" do # form method=GET => /org_search?search_terms
     
     Ohanakapa.configure do |config|
-        config.api_token = "7a7031966c424391bbab9900fcf3fa0a" # = ENV['OHANA_API_TOKEN'] if ENV['OHANA_API_TOKEN'].present?
-        config.api_endpoint = "https://ohana-api.herokuapp.com/api/" # = ENV['OHANA_API_ENDPOINT']
+        # DEVELOPMENT
+        config.api_token = "7a7031966c424391bbab9900fcf3fa0a" 
+        config.api_endpoint = "https://ohana-api.herokuapp.com/api/" 
+        
+        # PRODUCTION
+        # config.api_token = ENV['OHANA_API_TOKEN'] if ENV['OHANA_API_TOKEN'].present?
+        # config.api_endpoint = ENV['OHANA_API_ENDPOINT']
     end
     
     @search = Ohanakapa.search("search", :keyword => params[:search_terms])
+    @search.sort_by! do |org|
+        org.organization.name
+    end
+    @refine_by = {
+        "zip" => [],
+        "city" => []
+        }
+    
+    def get_refine_by(search)
+        search.each do |org|
+            if @refine_by["zip"].include?(org.address.postal_code) == false
+                @refine_by["zip"] << org.address.postal_code[0..4]
+            end
+            if @refine_by["city"].include?(org.address.city) == false
+                @refine_by["city"] << org.address.city
+            end
+        end
+        @refine_by["zip"].sort!
+        @refine_by["city"].sort!    
+    end   
+    get_refine_by(@search)
+    
     erb(:show_search_results)
+    
 end
